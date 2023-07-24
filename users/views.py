39d47354ab21392e268bad7f8a2c6cd3a668a122
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-24 19:18:28 +0900

    users API뷰 작성

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+
+from users.models import MyUser
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def user_list(request, ):
+	users = MyUser.objects.all().order_by('username')
+	serializer = UserSerializer(instance=users, many=True)
+	from rest_framework.response import Response
+	return Response(serializer.data)
+
