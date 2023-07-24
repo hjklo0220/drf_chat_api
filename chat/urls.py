8baf0548c3f3065cf951bf9ff968d3e9d379d3ab
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-24 19:18:21 +0900

    chat app url 라우팅

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from chat import views
+
+urlpatterns = [
+	path('start/', views.start_convo, name='start_convo'),
+	path('<int:convo_id>', views.get_conversation, name='get_conversation'),
+	path('', views.conversations, name='conversations')
+
+]
+
