11297fe72f741f681af5bfb697af1e8db08c51bf
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-24 22:12:56 +0900

    ProtocalTypeRouter에 Wensocket추가

--- drf_chat_api/asgi.py
+++ drf_chat_api/asgi.py
@@ -9,12 +9,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from chat import routing
+from drf_chat_api.tokenauth_middleware import TokenAuthMiddlewate
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_chat_api.settings')
 
 application = ProtocolTypeRouter({
 	"http": get_asgi_application(),
+	"websocket": AllowedHostsOriginValidator(
+		TokenAuthMiddlewate(URLRouter(routing.websocket_urlpatterns))
+	)
 
 })
